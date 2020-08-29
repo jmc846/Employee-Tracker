48ecfff06adbc4ef76a3284ecc6e61462c3c51e7
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-08-29 12:10:48 -0400

    updates error to sql

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "root",
+    password: "67504@jc",
     database: "employee_db"
 });
 
