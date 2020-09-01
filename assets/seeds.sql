a2871601b64fbf9ff0db40bf17c1de65c82db66d
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-09-01 15:06:48 -0400

    changes to syntax for seeds and schema

--- assets/seeds.sql
+++ assets/seeds.sql
@@ -1,15 +1,15 @@
-USE employee_db;
+USE employeeTracker_DB;
 
-INSERT INTO department (name) VALUES ("Research & Development");
-INSERT INTO department (name) VALUES ("BioTech");
-INSERT INTO department (name) VALUES ("IT Services");
+INSERT INTO department (department_name) VALUES ("Research & Development");
+INSERT INTO department (department_name) VALUES ("BioTech");
+INSERT INTO department (department_name) VALUES ("IT Services");
 
-INSERT INTO roles (title, salary, department_id) VALUES ("Chief Bio-Analyst", 120, 2);
-INSERT INTO roles (title, salary, department_id) VALUES ("Research Associate", 90, 1);
-INSERT INTO roles (title, salary, department_id) VALUES ("Lab Tech", 150, 2);
-INSERT INTO roles (title, salary, department_id) VALUES ("IT helpdesk analyst", 65, 3);
-INSERT INTO roles (title, salary, department_id) VALUES ("Assistant of Bio-diversity", 120, 2);
-INSERT INTO roles (title, salary, department_id) VALUES ("COO Development", 65, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Chief Bio-Analyst", 120, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Research Associate", 90, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Lab Tech", 150, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("IT helpdesk analyst", 65, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Assistant of Bio-diversity", 120, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("COO Development", 65, 1);
 
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Hank ", "Azaria", 1);
 INSERT INTO employee (first_name, last_name, role_id) VALUES ("Yogi", "Berra", 4);
