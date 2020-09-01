a332d41e689f46691184ed44a4156be081ffba0a
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-09-01 15:30:05 -0400

    updates to roles vs role

--- assets/schema.sql
+++ assets/schema.sql
@@ -1,42 +1,32 @@
 DROP DATABASE IF EXISTS employeeTracker_DB;
 CREATE DATABASE employeeTracker_DB;
-
-USE employeeTracker_DB
-
+USE employeeTracker_DB;
 CREATE TABLE department
 (
     id INTEGER NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)    
-
 );
-
-    CREATE TABLE role
-    (
+CREATE TABLE role
+(
     id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL
-    department_id INTEGER    
-    PRIMARY KEY(id)  
-    FOREIGN KEY(department_id) REFERENCES(department.id)  
-
+    salary DECIMAL,
+    department_id INTEGER,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id) REFERENCES department(id)  
 );
-
-        CREATE TABLE employee
-        (
-        id INTEGER NOT NULL AUTO_INCREMENT,
-   first_name VARCHAR(30) NOT NULL
-   last_name VARCHAR(30)  NOT NULL
-   role_id INTEGER
-   manager_id INTEGER 
-   FOREIGN KEY(manager_id) REFERENCES(employee.id),
-   FOREIGN KEY(role_id) REFERENCES(roles.id)
+CREATE TABLE employee
+(
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30)  NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY(manager_id) REFERENCES employee(id),
+    FOREIGN KEY(role_id) REFERENCES role(id),
     PRIMARY KEY(id)    
-
 );
-
-
-
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
